e7beb328a66de0f119b7201bb88f2ccdc4317aa6
azizkhonteacher <azizkhonteacher@gmain.com>
2025-01-27 13:46:59 +0500

    update

--- services/services.js
+++ services/services.js
@@ -1,4 +1,7 @@
-const baseURL = "http://localhost:8888";
+const baseURL = process.env.NODE_ENV === "production" 
+  ? "https://your-netlify-site.netlify.app" // Netlify domeni
+  : "http://localhost:8888"; // Lokal server uchun
+
 export default {
   getPosts() {
     return $fetch(`${baseURL}/posts`);
